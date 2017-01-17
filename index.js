5d8c167cf3660c13817baa2811a20b1aaf29b3f2
Daniel Toro <danieltsx@iMac-de-Daniel.local>
2017-01-17 02:07:04 -0300

    Uso de verbos de HTTP

--- index.js
+++ index.js
@@ -3,15 +3,28 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
-let app = express();
+let app = express()
 
 let port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.get('/hola/:name', (req, res) => {
-  res.send({message: `Hola ${req.params.name}!`})
+app.get('/api/product', (req, res) => {
+  res.status(200).send({products: []})
+})
+app.get('/api/product/:productId', (req, res) => {
+
+})
+app.post('/api/product', (req, res) => {
+  console.log(req.body)
+  res.status(200).send({message: 'El producto se ha recibido'})
+})
+app.put('/api/product/:productId', (req, res) => {
+
+})
+app.delete('/api/product/productId', (req, res) => {
+
 })
 
 app.listen(port, () => {
