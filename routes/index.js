e145883d0c99dd6e3f2beb98f8a54a32af933156
Daniel Toro <danieltsx@iMac-de-Daniel.local>
2017-01-17 16:45:51 -0300

    Protegiendo rutas con Web Tokens

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const productController = require('../controllers/product')
+const auth = require('../middlewares/auth');
 const api = express.Router()
 
 api.get('/product', productController.getProducts)
@@ -9,5 +10,8 @@ api.get('/product/:productId', productController.getProduct)
 api.post('/product', productController.saveProduct)
 api.put('/product/:productId', productController.updateProduct)
 api.delete('/product/:productId', productController.deleteProduct)
+api.get('/private', function(req, res){
+  res.status(200).send({message: `Tienes acceso`})
+})
 
 module.exports = api
