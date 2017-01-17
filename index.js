a82e36ec3045e3f3afaa4f79df58415d49bdbe25
Daniel Toro <danieltsx@iMac-de-Daniel.local>
2017-01-17 02:53:45 -0300

    Agregando conexion con mongodb a traves de mongoose

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 let app = express()
 
@@ -26,7 +27,13 @@ app.put('/api/product/:productId', (req, res) => {
 app.delete('/api/product/productId', (req, res) => {
 
 })
+mongoose.connect('mongodb://localhost:27017/shop', (err, res) => {
+  if(err) {
+    return console.log(`Error al conectar a la base de datos: ${err}`)
+  }
+  console.log('Conexion a la base de datos establecida...')
 
-app.listen(port, () => {
-  console.log(`API REST corriendo en http://localhost:${port}`)
+  app.listen(port, () => {
+    console.log(`API REST corriendo en http://localhost:${port}`)
+  })
 })
