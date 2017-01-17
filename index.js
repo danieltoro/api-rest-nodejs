99d1a9a31e391753b61a8a06ea8dc149ebb3fc0f
Daniel Toro <danieltsx@iMac-de-Daniel.local>
2017-01-17 03:54:46 -0300

    Implementando todos los verbos de HTTP

--- index.js
+++ index.js
@@ -14,11 +14,29 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.get('/api/product', (req, res) => {
-  res.status(200).send({products: []})
-})
-app.get('/api/product/:productId', (req, res) => {
+
+  Product.find({}, (err, products) => {
+    if(err) return res.status(500).send({message: `Error al realizar la peticion: ${err}`})
+    if(!products) return res.status(404).send({message: `No existen productos`})
+
+    res.status(200).send({products})
+  })
 
 })
+
+
+app.get('/api/product/:productId', (req, res) => {
+  let productId = req.params.productId
+
+  Product.findById(productId, (err, product) => {
+    if (err) return res.status(500).send({message: `Error al realizar la peticion: ${err}`})
+    if (!product) return res.status(404).send({message: `El producto no existe`})
+
+    res.status(200).send({product})
+  })
+})
+
+
 app.post('/api/product', (req, res) => {
   console.log('POST /api/product')
   console.log(req.body)
@@ -37,12 +55,33 @@ app.post('/api/product', (req, res) => {
     res.status(200).send({product: productStored})
   })
 })
+
+
 app.put('/api/product/:productId', (req, res) => {
+  let productId = req.params.productId
+  let update = req.body
 
-})
-app.delete('/api/product/productId', (req, res) => {
+  Product.findByIdAndUpdate(productId, update, (err, productUpdated) => {
+    if(err) res.status(500).send({message: `Error al actualizar el producto: ${err}`})
 
+    res.status(200).send({product: productUpdated})
+  })
 })
+
+
+app.delete('/api/product/:productId', (req, res) => {
+  let productId = req.params.productId
+
+  Product.findById(productId, (err, product) => {
+    if(err) res.status(500).send({message: `Error al borrar producto: ${err}`})
+
+    product.remove(err => {
+      res.status(200).send({message: `el producto ha sido eliminado`})
+    })
+  })
+})
+
+
 mongoose.connect('mongodb://localhost:27017/shop', (err, res) => {
   if(err) {
     return console.log(`Error al conectar a la base de datos: ${err}`)
