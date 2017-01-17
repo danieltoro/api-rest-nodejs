57b0a00a62f7893208631fb0bb473bfdacdf1477
Daniel Toro <danieltsx@iMac-de-Daniel.local>
2017-01-17 04:57:46 -0300

    Refactorizando el codigo

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+const api = require('./routes')
+
+const productController = require('./controllers/product')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use('/api', api)
+
+
+
+module.exports = app
