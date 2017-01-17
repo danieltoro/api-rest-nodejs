40a42ca7033b6b52cf4f65b7e0229e4986358712
Daniel Toro <danieltsx@iMac-de-Daniel.local>
2017-01-17 03:16:53 -0300

    Guardando datos en mongodb con POST

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ let app = express()
 
 let port = process.env.PORT || 3000;
 
+let Product = require('./models/product')
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
@@ -18,8 +20,22 @@ app.get('/api/product/:productId', (req, res) => {
 
 })
 app.post('/api/product', (req, res) => {
+  console.log('POST /api/product')
   console.log(req.body)
-  res.status(200).send({message: 'El producto se ha recibido'})
+
+  let product = new Product ()
+
+  product.name = req.body.name
+  product.picture = req.body.picture
+  product.price = req.body.price
+  product.category = req.body.category
+  product.description= req.body.description
+
+  product.save((err, productStored) => {
+    if (err) res.status(500).send({message: `error al guardar en la base de datos: ${err}`})
+
+    res.status(200).send({product: productStored})
+  })
 })
 app.put('/api/product/:productId', (req, res) => {
 
